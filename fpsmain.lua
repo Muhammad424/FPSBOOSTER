a806bbe631c795064ac5c73261fd6ee150c83219
Scripter <135837731+Muhammad424@users.noreply.github.com>
2024-04-19 17:33:06 +0700

    Update fpsmain.lua

--- fpsmain.lua
+++ fpsmain.lua
@@ -1,1 +1,11 @@
-game:GetService("RunService"):Set3dRenderingEnabled(false)
+if not setfpscap then return end
+
+local UserInputService, RunService = game:GetService("UserInputService"), game:GetService("RunService")
+
+UserInputService.WindowFocusReleased:Connect(function()
+	RunService:Set3dRenderingEnabled(false); setfpscap(5)
+end)
+
+UserInputService.WindowFocused:Connect(function()
+	RunService:Set3dRenderingEnabled(true); setfpscap(360)
+end)
